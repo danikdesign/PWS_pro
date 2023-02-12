d8594af2d2fa97251c226d484d0436ce290d9c7e
danikdesign <danik.newmail@gmail.com>
2023-02-12 18:31:37 +0200

    Created Client model

---
+++ db/migrate/20230212163032_create_clients.rb
@@ -0,0 +1,16 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :address
+      t.string :purifier_brand
+      t.integer :purifier_stages
+      t.boolean :putifier_pump
+      t.integer :purifier_tank
+
+      t.timestamps
+    end
+  end
+end
