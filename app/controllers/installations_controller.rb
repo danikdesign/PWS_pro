3572f37d17db0ef75daedf9b1aba16e7b11104e0
danikdesign <danik.newmail@gmail.com>
2023-02-20 22:01:05 +0200

    Fixed bug with creating new installation

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,14 +1,25 @@
 class InstallationsController < ApplicationController
-
+  before_action :set_client
   def new
+    @installation = @client.installations.build
   end
   def create
-    @installation = @client.installations.build :installation_create_params
+    @installation = @client.installations.build installation_create_params
+
+    if @installation.save
+      redirect_to clients_path
+    else
+      render :new
+    end
   end
 
   private
 
   def installation_create_params
     params.require(:installation).permit(:date, :pressure, :incoming_tds, :out_tds, :notes, :status)
+  end
+
+  def set_client
+    @client = Client.find params[:client_id]
   end
 end
