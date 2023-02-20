640a42947635d2d334401bab90df5d4f538765df
danikdesign <danik.newmail@gmail.com>
2023-02-20 23:56:29 +0200

    Created services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,45 @@
 # frozen_string_literal: true
 
-class ServicesController
+class ServicesController < ApplicationController
+  before_action :set_client
+  def new
+    @service = @client.services.build
+  end
+  def create
+    @service = @client.services.build service_create_params
+
+    if @service.save
+      respond_to do |format|
+        format.html do
+          flash[:success] = "New service has been added"
+          redirect_to clients_path
+        end
+
+        format.turbo_stream do
+          flash.now[:success] = "Water purifier has been added"
+        end
+      end
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def service_create_params
+    params.require(:service).permit(:date,
+                                    :replaced,
+                                    :pressure,
+                                    :incoming_tds,
+                                    :out_tds_before,
+                                    :out_tds_after,
+                                    :next_date,
+                                    :notes,
+                                    :status)
+  end
+
+  def set_client
+    @client = Client.find params[:client_id]
+  end
 end
