1617950ca50ee0d67cdb4201a240d372d7c47968
danikdesign <danik.newmail@gmail.com>
2023-02-20 23:46:56 +0200

    Added routes for services

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :clients do
     resources :installations, only: %i[new create edit update]
+    resources :services, only: %i[new create edit]
   end
 
   root 'pages#index'
