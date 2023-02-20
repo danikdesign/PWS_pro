1c268e21ecb2ed315e9027b6bc7dd9a7dd6e92e1
danikdesign <danik.newmail@gmail.com>
2023-02-20 22:19:34 +0200

    Added validates to installation model

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,4 +1,9 @@
 class Installation < ApplicationRecord
   belongs_to :client
   has_one :ticket
+
+  validates :date, presence: true
+  validates :pressure, presence: true
+  validates :incoming_tds, presence: true
+  validates :out_tds, presence: true
 end
