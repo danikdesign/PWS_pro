305e35a3494a6ef777f1bd94935af26f32b125ab
danikdesign <danik.newmail@gmail.com>
2023-02-27 19:33:12 +0200

    Created routes for Tickets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
 
   resources :clients do
     resources :installations, only: %i[new create edit update]
-    resources :services, only: %i[new create edit]
+    resources :services, only: %i[new create edit update]
+  end
+
+  resources :services, only: %i[new create edit update] do
+    resources :tickets
+  end
+
+  resources :installations, only: %i[new create edit update] do
+    resources :tickets
   end
 
   root 'pages#index'
