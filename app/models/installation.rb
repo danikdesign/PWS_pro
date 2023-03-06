dab5bde21429fd3158134e2cfa6fc344f3c1b835
danikdesign <danik.newmail@gmail.com>
2023-03-06 23:20:02 +0200

    Added installation date field in the client list

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -6,4 +6,8 @@ class Installation < ApplicationRecord
   validates :pressure, presence: true
   validates :incoming_tds, presence: true
   validates :out_tds, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["date"]
+  end
 end
