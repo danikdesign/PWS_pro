a825395d52d462e8abeb781f39105b65062711e6
danikdesign <danik.newmail@gmail.com>
2023-03-08 22:33:58 +0200

    Created fetch_parts method. Added purifier_parts to ServiceCreator call.

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,8 +1,11 @@
 class InstallationsController < ApplicationController
   before_action :set_client
+  before_action :fetch_parts, only: %i[create]
+  
   def new
     @installation = @client.installations.build
   end
+
   def create
     @installation = @client.installations.build installation_create_params
 
@@ -10,14 +13,14 @@ class InstallationsController < ApplicationController
       respond_to do |format|
         format.html do
           flash[:success] = "Water purifier has been added"
-          redirect_to clients_path
+          redirect_to client_path(@client)
         end
 
         format.turbo_stream do
           flash.now[:success] = "Water purifier has been added"
         end
       end
-      ServiceCreator.new(@client).call(@installation.date)
+      ServiceCreator.new(@client, @purifier_parts).call(@installation.date)
 
     else
       render :new, status: :unprocessable_entity
@@ -29,7 +32,7 @@ class InstallationsController < ApplicationController
   def installation_create_params
     params.require(:installation).permit(:date,
                                          :pressure,
-                                         :incoming_tds,
+                                         :in_tds,
                                          :out_tds,
                                          :notes,
                                          :status)
@@ -37,5 +40,10 @@ class InstallationsController < ApplicationController
 
   def set_client
     @client = Client.find params[:client_id]
+    @client.decorate
+  end
+
+  def fetch_parts 
+    @purifier_parts = PurifierPart.all
   end
 end
