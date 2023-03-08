22c3918751564eff267f3b56647d6155ff600b8c
danikdesign <danik.newmail@gmail.com>
2023-03-08 22:29:14 +0200

    Added purifier parts functional to service_creator

--- app/services/service_creator.rb
+++ app/services/service_creator.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ServiceCreator < ApplicationService
-  def initialize(client)
+  def initialize(client, purifier_parts)
     @client = client
+    @parts = purifier_parts
   end
 
   def call(date)
@@ -14,13 +15,20 @@ class ServiceCreator < ApplicationService
   protected
 
   def create_service(date)
+
+    if @client.services.size.odd? || @client.services.size == 0
+      @replacement = @parts[0..2]
+    else
+      @replacement = @parts[0..3]
+    end
+
     next_date = date + 182
     @client.services.create(date: next_date,
-                            replaced: '---',
                             pressure: 0.0,
-                            incoming_tds: 0,
+                            in_tds: 0,
                             out_tds_before: 0,
                             out_tds_after: 0,
-                            status: false)
+                            status: false,
+                            purifier_parts: @replacement)
   end
 end
