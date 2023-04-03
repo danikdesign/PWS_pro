88edbca8ec070de8c5f7f9d8f56c1e31bc972709
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-04-03 20:30:54 +0300

    Added validations test in installation spec

--- spec/models/installation_spec.rb
+++ spec/models/installation_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Installation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :pressure }
+    it { should validate_presence_of :in_tds }
+    it { should validate_presence_of :out_tds }
+  end
 end
