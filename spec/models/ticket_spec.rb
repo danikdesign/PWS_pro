0f69e6630128ca7ab43a27e2c3a0bf6940c37f3a
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-04-05 16:21:53 +0300

    Created Ticket spec with validations and associations tests

---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Ticket, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :datetime }
+  end
+    
+  describe 'associations' do
+    it { should belong_to :ticketable } 
+  end
+end
