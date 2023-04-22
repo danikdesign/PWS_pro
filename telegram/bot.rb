6f2ff18f266daf81a9aa8b1d8092a80a1cb7fb82
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-04-22 19:30:15 +0300

    Configure message sending from Telegram bot

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -1,5 +1,12 @@
-require File.expand_path('.../config/enviroment', __dir__)
+require File.expand_path('../config/environment', __dir__)
 
 require 'telegram/bot'
 
 token = '6218601166:AAFgMbEksJwJjN-f6Aoiout4PdSL3-N2_C8'
+
+Telegram::Bot::Client.run(token, environment: :test) do |bot|
+  bot.listen do |message|
+    p message
+    return
+  end
+end
