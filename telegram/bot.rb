dc4d1c9205343860b85b22523365bf85cd77caf7
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-04-25 12:09:37 +0300

    Added access to services for a week. Added scheduled notification of services every Monday

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -1,68 +1,55 @@
 require File.expand_path('../config/environment', __dir__)
 
 require 'telegram/bot'
-require_relative 'week_services.rb'
 require 'rufus-scheduler'
+require 'date'
 
 scheduler = Rufus::Scheduler.new
 
 token = ENV['TELEGRAM_BOT_TOKEN']
+chat_id = ENV['D_CHAT_ID']
 
 def week_services
   num = 0
   services = 
-    Service.in_a_week.map do |service|
-      num += 1
-      client = Client.find(service.client_id)
-      name = "#{client.first_name} #{client.last_name}"
-  
-      "#{num}. Date: #{service.date}\n#{name}\n\n"
+    if Service.in_a_week.present?
+      Service.in_a_week.map do |service|
+        num += 1
+        client = Client.find(service.client_id)
+        name = "#{client.first_name} #{client.last_name}"
+    
+        "#{num}. Date: #{service.date}\n#{name}\n\n"
+      end
+    else
+      ["No service this week"]
     end
 
   services.join
 end
 
 Telegram::Bot::Client.run(token) do |bot|
+  
+  scheduler.cron '0 12 * * 1 Europe/Kiev' do
+    bot.api.send_message(chat_id: chat_id, text: "#{week_services}")
+  end
+
+  
   bot.listen do |message|
 
     if !User.exists?(telegram_id: message.from.id)
       bot.api.send_message(chat_id: message.chat.id, text: "Sorry, but I can't find you in the system :(")
     else
-      case message.text
-      when '/start'
-        question = 'Выберите интересующий вас пункт меню:'
-
-        menu =
-          Telegram::Bot::Types::ReplyKeyboardMarkup.new(
-            keyboard: [
-              [{ text: 'Обслуживания на ближайшую неделю' }, { text: 'B' }],
-              [{ text: 'C' }, { text: '/stop' }]
-            ],
-            one_time_keyboard: true
-          )
-        bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: menu)
-      when 'Обслуживания на ближайшую неделю'
-        menu =
-        Telegram::Bot::Types::ReplyKeyboardMarkup.new(
-          keyboard: [
-            [{ text: 'Обслуживания на ближайшую неделю' }, { text: 'B' }],
-            [{ text: 'C' }, { text: '/stop' }]
-          ],
-          one_time_keyboard: true
-        )
-
-        bot.api.send_message(chat_id: message.chat.id, text: "#{week_services}", reply_markup: menu)
-      when 'B'
+      case message
+      when Telegram::Bot::Types::CallbackQuery
+        if message.data == 'services'
+          bot.api.send_message(chat_id: message.from.id, text: "#{week_services} #{Time.now}")
+        end
+      when Telegram::Bot::Types::Message
         kb = [[
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Give me your phone number', request_contact: true),
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Show me your location', request_location: true)
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Show', callback_data: 'services'),
         ]]
-        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
-        bot.api.send_message(chat_id: message.chat.id, text: 'Hey!', reply_markup: markup)
-      when '/stop'
-        # See more: https://core.telegram.org/bots/api#replykeyboardremove
-        kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-        bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
+        markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+        bot.api.send_message(chat_id: message.chat.id, text: 'Services in a week', reply_markup: markup)
       end
     end
   end
