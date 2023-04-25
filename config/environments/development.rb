e879a0e72b0c6098c444129808f4c6dff99571dc
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-04-25 16:15:34 +0300

    Turned off bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,15 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = false
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
