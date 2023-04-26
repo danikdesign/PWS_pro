27e78d23d0c0f0fe186f64f72f0d9b6a4a1fe3bd
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-04-26 19:59:20 +0300

    Added test routes for deploying

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,30 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
-    passwordless_for :users, at: '/', as: :auth
 
-    resources :clients do
-      resources :installations, only: %i[new create edit update]
-      resources :services, only: %i[new create edit update]
-    end
+  root to: redirect('/healthcheck', status: 302)
+  get "/healthcheck", to: proc { [200, {}, ["success"]] }
 
-    resources :services, only: %i[new create edit update] do
-      resources :tickets, only: %i[new create edit update destroy]
-    end
+  # scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+  #   passwordless_for :users, at: '/', as: :auth
 
-    resources :installations, only: %i[new create edit update] do
-      resources :tickets, only: %i[new create edit update destroy]
-    end
+  #   resources :clients do
+  #     resources :installations, only: %i[new create edit update]
+  #     resources :services, only: %i[new create edit update]
+  #   end
 
-    resources :tickets, only: %i[index show]
+  #   resources :services, only: %i[new create edit update] do
+  #     resources :tickets, only: %i[new create edit update destroy]
+  #   end
 
-    root 'pages#index'
+  #   resources :installations, only: %i[new create edit update] do
+  #     resources :tickets, only: %i[new create edit update destroy]
+  #   end
 
-    get 'services_calendar', to: 'pages#services_calendar'
-  end
+  #   resources :tickets, only: %i[index show]
+
+  #   root 'pages#index'
+
+  #   get 'services_calendar', to: 'pages#services_calendar'
+  # end
 end
