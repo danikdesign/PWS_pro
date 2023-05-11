812198edbc7c3581244c2a95aefc429fef945bed
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-05-11 21:16:09 +0300

    Added deliver now for passwordless auth mail

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -1,6 +1,6 @@
 Passwordless.after_session_save = lambda do |session, request|
   # Default behavior is
-  Passwordless::Mailer.magic_link(session).deliver_later
+  Passwordless::Mailer.magic_link(session).deliver_now
 
   # You can change behavior to do something with session model. For example,
   # session.authenticatable.send_sms
